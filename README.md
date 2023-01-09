f20ea7b3bbd46d4973751d630dc9d96a7688f3d3
Daniel Hoop <daniel.hoop@agroscope.admin.ch>
2023-01-09 20:08:09 +0100

    Added binary subtraction.

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
-# Binary addition
-This project implements a binary addition with AND, OR and XOR gates. Find the calculator here: https://danielhoop.github.io/binary-calculator/
+# Binary calculator
+This project implements binary addition and subtraction using exclusively AND, OR and XOR gates. Find the calculator here: https://danielhoop.github.io/binary-calculator/
 
+## Addition
 The following figure shows the circuit. Input of gates are at bottom/right, the output is at the top/left.
 * A = Figure 1 for addition
 * B = Figure 2 for addition
@@ -14,3 +15,19 @@ The following figure shows the circuit. Input of gates are at bottom/right, the 
 </table>
 
 Not sure, if this is the optimal circuit, as I designed it myself and did never study this subject before. I heard somewhere that calculations can be designed by using AND, OR and XOR gates. So, I just tried it and it seems to work. :-)
+
+
+## Subtraction
+The following figure shows the circuit. The direction of the control flow is indicated. It is a hot mess, I admit! ^^
+* A = Figure 1
+* B = Figure 2 will be subtracted from A
+* o = Overflow from the subtraction of two digits (or the previous ones)
+* s = The sign indicating if the result is negative
+* r = Final result from the subtraction of digits (taking into account the previous overflow)
+
+<table>
+<tr><td><img src="circuit-subtraction.jpg" alt="Circuit for binary subtraction"></img></td></tr>
+<tr><td>Circuit for binary subtraction. It contains an error in the calculation of the overflow. In the code, this deviation is marked with "DEVIATION FROM CIRCUIT".</td></tr>
+</table>
+
+This one was quite hard to think through. Here as well, I did not consult any teaching material. I'm a little proud, I must admit... It really works!!! :-)
--- script.js
+++ script.js
@@ -97,3 +97,93 @@ perform_addition = function() {
   aTxt + " = A\n" +
   bTxt + " = B\n";
 }
+
+
+perform_subtraction = function() {
+
+  if (document.getElementById("fig1s").value == "" && document.getElementById("fig2s").value == "") {
+    return null;
+  }
+
+  a = get_single_digits(document.getElementById("fig1s").value);
+  b = get_single_digits(document.getElementById("fig2s").value);
+  a = make_equal_length(a, b);
+  b = make_equal_length(b, a);
+  len = a.length;
+
+  // This bit says if this or lower bits are not empty.
+  // Thaht's needed to determine if the overflow should be used or if the
+  // result from the overflow should be put into the negative sign.
+  nEm = Array.apply(null, Array(len));
+  nEm[len - 1] = or(a[len - 1], b[len - 1]);
+  if (len > 1) {
+    for (let i = len - 2; i >= 0; i--) {
+      nEm[i] = or(nEm[i + 1], or(a[i], b[i]));
+    }
+  }
+
+  res0 = xor(a[0], b[0]);
+  ovf0 = and(
+    xor(a[0], 1),  // a is 0
+    xor(b[0], 0)); // and b is 1
+
+  res = [res0];
+  ovf = [ovf0];
+
+  for (let i = 1; i < len; i++) {
+    // Result
+    res[i] = and(
+      nEm[i], // Digit is not empty
+      xor( // ...and xor(a, b | overflow)
+        a[i],
+        or(b[i], ovf[i - 1]))
+      )
+    // Overflow
+    // DEVIATION FROM CIRCUIT. The overflow is not according to the circuit, i.e. the circuit is erroneous.
+    // Added the embracing or(...) and the line 'xor(and(b[i], ovf[i - 1]), 0)' in it.
+    // According to the circuit, it would only be the and(...) statement.
+    ovf[i] = or(
+      xor(and(b[i], ovf[i - 1]), 0), // b AND prev overflow are 1.
+      and(
+        xor(a[i], 1), // a is 0
+        xor(or(b[i], ovf[i - 1]), 0))  // and b or previous overflow is 1
+      );
+  }
+
+  // The sign
+  // The last sign will be handled specially because there is no "not empty" bit.
+  sig = Array.apply(null, Array(len));
+  for (let i = 0; i < len - 1; i++) {
+    sig[i] = and(
+      xor(nEm[i], 1), //  Current digits is empty
+      xor(ovf[i], 0)); // and overflow (from previous) is 1.
+  }
+  sig[len - 1] = ovf[len - 1]; // In the last digits, overflow equals sign.
+
+  // If any sign was given, then apply it to the result.
+  sign = 0;
+  for (let i = 0; i < len; i++) {
+    sign = or(sign, sig[i]);
+  }
+  sign = sig[len - 1] == 1 ? "-" : "";
+
+  resTxt = sign + reverse(res).join("");
+  ovfTxt = reverse(ovf).join("");
+  sigTxt = reverse(sig).join("");
+  aTxt = reverse(a).join("");
+  bTxt = reverse(b).join("");
+  if (sign == "-") {
+    ovfTxt = " " + ovfTxt;
+    sigTxt = " " + sigTxt;
+    aTxt = " " + aTxt;
+    bTxt = " " + bTxt;
+  }
+
+  document.getElementById("ress").value = 
+  resTxt + " = result \n" +
+  sigTxt + " = negative sign indication\n" +
+  ovfTxt + " = overflow\n" +
+  "---------------\n" +
+  aTxt + " = A\n" +
+  bTxt + " = B\n";
+}
